9b499d8e83ae34c59d69bbfe3254e1604c943f07
ranganathan-kr <141033656+ranganathan-kr@users.noreply.github.com>
2023-08-11 21:33:36 +0530

    EFCF1

---
+++ 10.8.2023_Task/EFCF1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCF1;
+
+//Console.WriteLine("Hello, World!");
+CURD curd = new CURD();
+//curd.AddNewDept();
+curd.UpdateDept();
+//curd.DeleteDept();
+
