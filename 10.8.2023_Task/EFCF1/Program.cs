4ae70cd29fd12d475879a0475833c09ef83665fd
ranganathan-kr <141033656+ranganathan-kr@users.noreply.github.com>
2023-08-12 20:26:24 +0530

    Delete Program.cs

--- 10.8.2023_Task/EFCF1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EFCF1;
-
-//Console.WriteLine("Hello, World!");
-CURD curd = new CURD();
-//curd.AddNewDept();
-curd.UpdateDept();
-//curd.DeleteDept();
-
