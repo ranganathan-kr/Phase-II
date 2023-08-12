59689944a04e7af715f604bbee8c07f5dfd86a85
ranganathan-kr <141033656+ranganathan-kr@users.noreply.github.com>
2023-08-12 20:25:35 +0530

    EFDF

---
+++ 10.8.2023_Task/EFDF/Program.cs
@@ -0,0 +1,11 @@
+﻿using EFDF;
+using EFDF.Models;
+
+CRUD crud =new CRUD();
+/*crud.InsertRecords();
+*/crud.delete();
+
+CRUD_Emp emp= new CRUD_Emp();
+emp.InsertEmp();
+/*emp.deleteEmp();
+emp.updateEmp();*/
