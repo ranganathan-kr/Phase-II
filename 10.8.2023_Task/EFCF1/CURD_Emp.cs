3b60ace3d1e6446d22e0f5c08322d0103eee5bd0
ranganathan-kr <141033656+ranganathan-kr@users.noreply.github.com>
2023-08-12 20:27:31 +0530

    EFCF1

---
+++ 10.8.2023_Task/EFCF1/CURD_Emp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCF1.Context;
+using EFCF1.Model;
+
+namespace EFCF1
+{
+    internal class CURD_Emp
+    {
+        public void AddNewEmp()
+        {
+            using (var context = new EmployeeManagement())
+            {
+                var emp = new Emp()
+                {
+                    Empno = 101,
+                    Ename = "Alien",
+                    Addr = "Mars"
+                };
+                context.Emps.Add(emp);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateEmp()
+        {
+            var context = new EmployeeManagement();
+            var selEmp = context.Emps.FirstOrDefault(d => d.Empno== 101);
+            selEmp.Ename = "Human";
+            context.SaveChanges();
+
+
+        }
+        public void DeleteEmp()
+        {
+
+            var context = new EmployeeManagement();
+            var selEmp = context.Emps.FirstOrDefault(d => d.Empno == 101);
+            context.Emps.Remove(selEmp);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ 10.8.2023_Task/EFCF1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCF1;
+
+//Console.WriteLine("Hello, World!");
+CURD curd = new CURD();
+//curd.AddNewDept();
+curd.UpdateDept();
+//curd.DeleteDept();
+CURD_Emp emp = new CURD_Emp();
+// emp.AddNewEmp();
+// emp.UpdateEmp();
+emp.DeleteEmp();
