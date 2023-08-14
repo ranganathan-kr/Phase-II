1fef8525113f619612b993869edab5e4c7f09cf3
ranganathan-kr <141033656+ranganathan-kr@users.noreply.github.com>
2023-08-15 01:21:05 +0530

    Add files via upload

---
+++ 12.8.2023_Task/12.8.2023_Task EFCF/Golbal _Exception/StudentDetailsException.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentDetails.Global_Exception
+{
+    public static class StudentDetailsException 
+    {
+
+        public static List<string>MyException { get; } =
+        new List<string>
+
+        {
+            "Student Not Found"
+        };
+
+    }
+}
---
+++ 12.8.2023_Task/12.8.2023_Task EFCF/Services/Interfaces/IStudent.cs
@@ -0,0 +1,17 @@
+﻿using StudentDetails.Model;
+
+namespace StudentDetails.Services.Interfaces
+{
+    public interface IStudent
+    {
+
+         Task <List<Student>> GetAllStudents();
+
+         Task<Student> GetStudentById(int id);
+
+         Task<List<Student>> AddStudent(Student student);
+         Task<Student> UpdateStudent(int Rollno,Student student);
+         Task<Student> DeleteStudent(int Rollno);
+
+    }
+}
